24930fb124062eaf5e4decf8d761308771447f19
mertcebe <mr.mertcebe@gmail.com>
2024-07-27 15:52:24 +0300

    create firebase doc

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDTmFU4ne226lbN9dqjyVmupZk4MThFODA",
+    authDomain: "realtor-app-42e67.firebaseapp.com",
+    projectId: "realtor-app-42e67",
+    storageBucket: "realtor-app-42e67.appspot.com",
+    messagingSenderId: "747331104412",
+    appId: "1:747331104412:web:6b89541fcd8bdf398a8759",
+    measurementId: "G-3JZML7SLHG"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const database = getFirestore();
+
+const auth = getAuth();
+
+export { database as default, auth }
